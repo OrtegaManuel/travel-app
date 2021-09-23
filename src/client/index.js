bdf70020e3269dbe5accae8579b4c829c9b23058
OrtegaManuel <manuel-ortega@posteo.de>
2021-09-23 17:02:57 +0200

    feat(added sass)

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import { getWeatherData } from './js/app';
 import { getWeatherURL } from './js/app';
 import { postData } from './js/app';
 import { updateUI } from './js/app';
+import './styles/style.scss';
 
 alert('I EXIST!');
