75441308f6754aac7a534799d120880525c036c4
OrtegaManuel <manuel-ortega@posteo.de>
2021-10-13 15:07:57 +0200

    feat(small css changes for print feature)

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -18,13 +18,13 @@ function getCountryData(event) {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify({ city: cityInput, date: dateInput }), // sending to server.js
+      body: JSON.stringify({ city: cityInput, date: dateInput }), // sending values to server.js
     })
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(function (res) {
         document.getElementById(
           'cityResult'
-        ).innerHTML = `Your destination is ${cityInput}`;
+        ).innerHTML = `Your trip to ${cityInput} starts on ${dateInput}`;
         document.getElementById(
           'coords'
         ).innerHTML = `The coordinates for ${cityInput} are: ${res.city.lat},${res.city.lng}`;
@@ -33,10 +33,17 @@ function getCountryData(event) {
         ).innerHTML = `The city of ${cityInput} belongs to: ${res.city.country}`;
         document.getElementById(
           'temp'
-        ).innerHTML = `The temperature is: ${res.weather.temp}`;
+        ).innerHTML = `The forecast is: ${res.weather.temp}°C - ${res.weather.weatherDescription}`;
         document.getElementById(
           'cityImage'
         ).innerHTML = `<img src="${res.cityImage.webformatURL}"class='result_img'/>`;
+        document.getElementById('print--btn').innerHTML = `<input
+        class="print--btn"
+        type="button"
+        name="print"
+        value="Print Result"
+        onClick="window.print()"
+    />`;
       });
   }
 }
